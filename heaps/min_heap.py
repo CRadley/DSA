8cda7183506bf95a8d4ba73429aa6d4bfe751ba7
Connor Radley <connor.radley@hotmail.co.uk>
2024-01-13 21:32:20 +0000

    Move determine_child_indices and determine_parent_index to heaps.utils module

--- heaps/min_heap.py
+++ heaps/min_heap.py
@@ -1,12 +1,5 @@
 from typing import List
-
-
-def determine_child_indices(n: int) -> (int, int):
-    return 2 * n + 1, 2 * n + 2
-
-
-def determine_parent_index(n: int) -> int:
-    return (n - 1) // 2
+from .utils import determine_child_indices, determine_parent_index
 
 
 class MinHeap:
---
+++ heaps/utils.py
@@ -0,0 +1,6 @@
+def determine_child_indices(n: int) -> (int, int):
+    return 2 * n + 1, 2 * n + 2
+
+
+def determine_parent_index(n: int) -> int:
+    return (n - 1) // 2
